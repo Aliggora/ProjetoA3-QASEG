d31a083fad1c2bb4a3cc59c950dfabb6c77f03dc
Gustavo Vieira <65238669+Aliggora@users.noreply.github.com>
2023-06-18 01:57:43 -0300

    Acessos e cadastros
    Essa versão do sistema permite a criação de acessos e visualização de chamados abertos anteriormente. (Necessário ajustar os CSVs)

--- src/sistemaChamados/CriarChamado.java
+++ src/sistemaChamados/CriarChamado.java
@@ -4,12 +4,14 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Arrays;
-import java.util.List;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
 
 public class CriarChamado {
     private JFrame frame;
-    private JComboBox<String> tipoComboBox;
+    private JTextField assuntoTextField;
     private JTextArea descricaoTextArea;
 
     public CriarChamado() {
@@ -18,15 +20,15 @@ public class CriarChamado {
 
     private void initialize() {
         frame = new JFrame();
-        frame.setBounds(100, 100, 450, 300);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setBounds(100, 100, 400, 300);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frame.getContentPane().setLayout(new BorderLayout(0, 0));
 
         JPanel panel = new JPanel();
         frame.getContentPane().add(panel, BorderLayout.CENTER);
         panel.setLayout(new GridBagLayout());
 
-        JLabel lblNewLabel = new JLabel("Tipo de Chamado:");
+        JLabel lblNewLabel = new JLabel("Assunto:");
         GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
         gbc_lblNewLabel.anchor = GridBagConstraints.WEST;
         gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
@@ -34,56 +36,52 @@ public class CriarChamado {
         gbc_lblNewLabel.gridy = 0;
         panel.add(lblNewLabel, gbc_lblNewLabel);
 
-        tipoComboBox = new JComboBox<>();
-        GridBagConstraints gbc_tipoComboBox = new GridBagConstraints();
-        gbc_tipoComboBox.fill = GridBagConstraints.HORIZONTAL;
-        gbc_tipoComboBox.insets = new Insets(0, 0, 5, 0);
-        gbc_tipoComboBox.gridx = 1;
-        gbc_tipoComboBox.gridy = 0;
-        panel.add(tipoComboBox, gbc_tipoComboBox);
+        assuntoTextField = new JTextField();
+        GridBagConstraints gbc_assuntoTextField = new GridBagConstraints();
+        gbc_assuntoTextField.insets = new Insets(0, 0, 5, 0);
+        gbc_assuntoTextField.fill = GridBagConstraints.HORIZONTAL;
+        gbc_assuntoTextField.gridx = 1;
+        gbc_assuntoTextField.gridy = 0;
+        panel.add(assuntoTextField, gbc_assuntoTextField);
+        assuntoTextField.setColumns(10);
 
         JLabel lblNewLabel_1 = new JLabel("Descrição:");
         GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
-        gbc_lblNewLabel_1.anchor = GridBagConstraints.WEST;
-        gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
+        gbc_lblNewLabel_1.anchor = GridBagConstraints.NORTHWEST;
+        gbc_lblNewLabel_1.insets = new Insets(0, 0, 0, 5);
         gbc_lblNewLabel_1.gridx = 0;
         gbc_lblNewLabel_1.gridy = 1;
         panel.add(lblNewLabel_1, gbc_lblNewLabel_1);
 
         descricaoTextArea = new JTextArea();
-        descricaoTextArea.setRows(5); // Define o número de linhas exibidas
-        descricaoTextArea.setLineWrap(true); // Habilita a quebra de linha automática
-        JScrollPane scrollPane = new JScrollPane(descricaoTextArea); // Adiciona uma barra de rolagem
         GridBagConstraints gbc_descricaoTextArea = new GridBagConstraints();
-        gbc_descricaoTextArea.insets = new Insets(0, 0, 5, 0);
         gbc_descricaoTextArea.fill = GridBagConstraints.BOTH;
         gbc_descricaoTextArea.gridx = 1;
         gbc_descricaoTextArea.gridy = 1;
-        panel.add(scrollPane, gbc_descricaoTextArea);
+        panel.add(new JScrollPane(descricaoTextArea), gbc_descricaoTextArea);
 
-        JButton enviarButton = new JButton("Enviar");
-        enviarButton.addActionListener(new ActionListener() {
+        JButton criarButton = new JButton("Criar");
+        criarButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                String tipo = (String) tipoComboBox.getSelectedItem();
+                String assunto = assuntoTextField.getText();
                 String descricao = descricaoTextArea.getText();
+                String dataHora = LocalDateTime.now().toString();
 
-                Chamado chamado = new Chamado(tipo, descricao);
-                ArmazenaChamado.salvarChamado(chamado);
+                // Salva as informações em "chamados.csv"
+                try (PrintWriter writer = new PrintWriter(new FileWriter("chamados.csv", true))) {
+                    writer.println("Assunto: " + assunto);
+                    writer.println("Descrição: " + descricao);
+                    writer.println("Data/Hora: " + dataHora);
+                    writer.println();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
 
-                JOptionPane.showMessageDialog(frame, "Chamado aberto com sucesso!");
-                descricaoTextArea.setText("");
+                JOptionPane.showMessageDialog(frame, "Chamado criado com sucesso!");
+                frame.dispose();
             }
         });
-        GridBagConstraints gbc_enviarButton = new GridBagConstraints();
-        gbc_enviarButton.gridx = 1;
-        gbc_enviarButton.gridy = 2;
-        panel.add(enviarButton, gbc_enviarButton);
-
-        List<String> tiposChamado = Arrays.asList("Hardware", "Software", "Telefonia Movel",
-                "Telefonia Fixa", "Internet", "File Server");
-        for (String tipo : tiposChamado) {
-            tipoComboBox.addItem(tipo);
-        }
+        frame.getContentPane().add(criarButton, BorderLayout.SOUTH);
     }
 
     public void show() {
--- src/sistemaChamados/Main.java
+++ src/sistemaChamados/Main.java
@@ -4,6 +4,9 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     private JFrame frame;
@@ -56,7 +59,7 @@ public class Main {
         gbc_passwordField.gridy = 1;
         panel.add(passwordField, gbc_passwordField);
 
-        JButton loginButton = new JButton("Entrar");
+        JButton loginButton = new JButton("Enviar");
         loginButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 String username = usernameTextField.getText();
@@ -65,16 +68,16 @@ public class Main {
                 if (username.equals("admin") && new String(password).equals("admin")) {
                     JOptionPane.showMessageDialog(frame, "Admin logado com sucesso!");
                     frame.dispose();
-                    openTicketCreation();
+                    openAdminOptions();
                 } else if (username.equals("user") && new String(password).equals("user")) {
                     JOptionPane.showMessageDialog(frame, "User logado com sucesso!");
                     frame.dispose();
-                    openTicketCreation();
+                    openUserOptions();
                 } else {
                     JOptionPane.showMessageDialog(frame, "Usuário ou senha incorretos");
                 }
 
-                // Clear the password field
+                // Limpa o textbox de senha.
                 passwordField.setText("");
             }
         });
@@ -85,9 +88,166 @@ public class Main {
         frame.setVisible(true);
     }
 
+    private void openAdminOptions() {
+        JFrame adminFrame = new JFrame();
+        adminFrame.setBounds(100, 100, 400, 300);
+        adminFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        adminFrame.getContentPane().setLayout(new BorderLayout(0, 0));
+
+        JPanel panel = new JPanel();
+        adminFrame.getContentPane().add(panel, BorderLayout.CENTER);
+        panel.setLayout(new GridBagLayout());
+
+        JButton verificarChamadosButton = new JButton("Verificar chamados abertos");
+        verificarChamadosButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                List<String> chamadosAbertos = readChamadosCSV();
+                String chamadosString = String.join("\n", chamadosAbertos);
+                JOptionPane.showMessageDialog(adminFrame, "Chamados abertos:\n" + chamadosString);
+            }
+        });
+        GridBagConstraints gbc_verificarChamadosButton = new GridBagConstraints();
+        gbc_verificarChamadosButton.insets = new Insets(0, 0, 5, 0);
+        gbc_verificarChamadosButton.gridx = 0;
+        gbc_verificarChamadosButton.gridy = 0;
+        panel.add(verificarChamadosButton, gbc_verificarChamadosButton);
+
+        JButton cadastrarMembrosButton = new JButton("Cadastrar membros de equipe");
+        cadastrarMembrosButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String nomeCompleto = JOptionPane.showInputDialog(adminFrame, "Digite o nome completo:");
+                String re = JOptionPane.showInputDialog(adminFrame, "Digite o RE do funcionário:");
+                String nomeUsuario = JOptionPane.showInputDialog(adminFrame, "Digite o nome de usuário:");
+                String senhaAcesso = JOptionPane.showInputDialog(adminFrame, "Digite a senha de acesso:");
+
+                // Salvar as informações em "usuarios.csv"
+
+                JOptionPane.showMessageDialog(adminFrame, "Membro de equipe cadastrado com sucesso!");
+            }
+        });
+        GridBagConstraints gbc_cadastrarMembrosButton = new GridBagConstraints();
+        gbc_cadastrarMembrosButton.insets = new Insets(0, 0, 5, 0);
+        gbc_cadastrarMembrosButton.gridx = 0;
+        gbc_cadastrarMembrosButton.gridy = 1;
+        panel.add(cadastrarMembrosButton, gbc_cadastrarMembrosButton);
+
+        JButton cadastrarClientesButton = new JButton("Cadastrar clientes");
+        cadastrarClientesButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String nomeCliente = JOptionPane.showInputDialog(adminFrame, "Digite o nome do cliente:");
+                String cnpj = JOptionPane.showInputDialog(adminFrame, "Digite o CNPJ do cliente:");
+                String nomeUsuario = JOptionPane.showInputDialog(adminFrame, "Digite o nome de usuário:");
+                String senhaAcesso = JOptionPane.showInputDialog(adminFrame, "Digite a senha de acesso:");
+
+                // Salvar as informações em "clientes.csv"
+
+                JOptionPane.showMessageDialog(adminFrame, "Cliente cadastrado com sucesso!");
+            }
+        });
+        GridBagConstraints gbc_cadastrarClientesButton = new GridBagConstraints();
+        gbc_cadastrarClientesButton.insets = new Insets(0, 0, 5, 0);
+        gbc_cadastrarClientesButton.gridx = 0;
+        gbc_cadastrarClientesButton.gridy = 2;
+        panel.add(cadastrarClientesButton, gbc_cadastrarClientesButton);
+
+        JButton abrirChamadoButton = new JButton("Abrir chamado");
+        abrirChamadoButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                openTicketCreation();
+            }
+        });
+        GridBagConstraints gbc_abrirChamadoButton = new GridBagConstraints();
+        gbc_abrirChamadoButton.insets = new Insets(0, 0, 5, 0);
+        gbc_abrirChamadoButton.gridx = 0;
+        gbc_abrirChamadoButton.gridy = 3;
+        panel.add(abrirChamadoButton, gbc_abrirChamadoButton);
+
+        JButton desconectarButton = new JButton("Desconectar");
+        desconectarButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                adminFrame.dispose();
+                Main login = new Main();
+                login.show();
+            }
+        });
+        GridBagConstraints gbc_desconectarButton = new GridBagConstraints();
+        gbc_desconectarButton.gridx = 0;
+        gbc_desconectarButton.gridy = 4;
+        panel.add(desconectarButton, gbc_desconectarButton);
+
+        adminFrame.setVisible(true);
+    }
+
+    private void openUserOptions() {
+        JFrame userFrame = new JFrame();
+        userFrame.setBounds(100, 100, 400, 200);
+        userFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        userFrame.getContentPane().setLayout(new BorderLayout(0, 0));
+
+        JPanel panel = new JPanel();
+        userFrame.getContentPane().add(panel, BorderLayout.CENTER);
+        panel.setLayout(new GridBagLayout());
+
+        JButton abrirChamadoButton = new JButton("Abrir novo chamado");
+        abrirChamadoButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                openTicketCreation();
+            }
+        });
+        GridBagConstraints gbc_abrirChamadoButton = new GridBagConstraints();
+        gbc_abrirChamadoButton.insets = new Insets(0, 0, 5, 0);
+        gbc_abrirChamadoButton.gridx = 0;
+        gbc_abrirChamadoButton.gridy = 0;
+        panel.add(abrirChamadoButton, gbc_abrirChamadoButton);
+
+        JButton verificarStatusButton = new JButton("Verificar status de chamado");
+        verificarStatusButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                List<String> chamadosUsuario = readChamadosCSV();
+                String ultimoChamado = chamadosUsuario.get(chamadosUsuario.size() - 1);
+                JOptionPane.showMessageDialog(userFrame, "Último chamado aberto:\n" + ultimoChamado);
+            }
+        });
+        GridBagConstraints gbc_verificarStatusButton = new GridBagConstraints();
+        gbc_verificarStatusButton.insets = new Insets(0, 0, 5, 0);
+        gbc_verificarStatusButton.gridx = 0;
+        gbc_verificarStatusButton.gridy = 1;
+        panel.add(verificarStatusButton, gbc_verificarStatusButton);
+
+        JButton desconectarButton = new JButton("Desconectar");
+        desconectarButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                userFrame.dispose();
+                Main login = new Main();
+                login.show();
+            }
+        });
+        GridBagConstraints gbc_desconectarButton = new GridBagConstraints();
+        gbc_desconectarButton.gridx = 0;
+        gbc_desconectarButton.gridy = 2;
+        panel.add(desconectarButton, gbc_desconectarButton);
+
+        userFrame.setVisible(true);
+    }
+
     private void openTicketCreation() {
         CriarChamado criarChamado = new CriarChamado();
         criarChamado.show();
+    }
+
+    private List<String> readChamadosCSV() {
+        List<String> chamados = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader("chamados.csv"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                chamados.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return chamados;
     }
 
     public static void main(String[] args) {
